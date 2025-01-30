bfcff3bbd709dcc63ab04479c20e3970115a6732
Olaf Siebert <olaf@biffnewmedia.com>
2025-01-30 10:08:41 +0100

    feat: add blog post to site

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,36 +1,67 @@
 import { MetadataRoute } from 'next';
+import { createClient } from '@/prismicio';
 
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const client = createClient();
+
+  const posts = await client
+    .getByType('posts', {
+      pageSize: 100,
+      page: 0,
+      fetchLinks: ['author.name', 'author.profile_image', 'post_category.name', 'post_category.uid'],
+      orderings: [
+        {
+          field: 'my.posts.published_date',
+          direction: 'desc',
+        },
+      ],
+    })
+    .then(response => {
+      return response.results;
+    })
+    .catch(() => []);
+
+  const blogPosts = posts.map(post => {
+    return {
+      url: `https://${process.env.NEXT_PUBLIC_BASE_URL}/blog/${post.uid}`,
+      lastModified: post.data.publishing_date?.toString() ?? new Date().toISOString(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    };
+  });
+
+  const main = [
     {
       url: `https://${process.env.NEXT_PUBLIC_BASE_URL}`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
+      lastModified: new Date().toISOString(),
+      changeFrequency: 'yearly',
       priority: 1,
     },
     {
       url: `https://${process.env.NEXT_PUBLIC_BASE_URL}/about-us`,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
       changeFrequency: 'yearly',
       priority: 0.8,
     },
     {
       url: `https://${process.env.NEXT_PUBLIC_BASE_URL}/resources`,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
       changeFrequency: 'yearly',
       priority: 0.8,
     },
     {
       url: `https://${process.env.NEXT_PUBLIC_BASE_URL}/blog`,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
       changeFrequency: 'yearly',
       priority: 0.8,
     },
     {
       url: `https://${process.env.NEXT_PUBLIC_BASE_URL}/contact`,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
       changeFrequency: 'yearly',
       priority: 0.8,
     },
   ];
+
+  return [...main, ...blogPosts] as MetadataRoute.Sitemap;
 }
