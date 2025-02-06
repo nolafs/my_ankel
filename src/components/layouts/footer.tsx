43f0a91e924641228ab2fe5e22ea08ec021345ef
Olaf Siebert <olaf@biffnewmedia.com>
2025-02-06 18:10:49 +0100

    feat: blog system improvement

--- src/components/layouts/footer.tsx
+++ src/components/layouts/footer.tsx
@@ -25,7 +25,7 @@ export function Footer({ navigation, logo, footerCta, secondaryNavigation, socia
   const copyRightDate = new Date().getFullYear();
   return (
     <footer>
-      <div className="relative bg-[#516fdc] bg-gradient-to-b from-[#889de7] to-[#1c348c]">
+      <div className="relative bg-[#516fdc] bg-gradient-to-b from-[#3c5dd8] to-[#1c348c]">
         <div className="absolute inset-2 z-0 rounded-4xl border-transparent bg-white/15 shadow-md ring-1 ring-[#8fde5d]/15 after:absolute after:inset-0 after:rounded-4xl after:shadow-[inset_0_0_2px_1px_#ffffff4d]" />
 
         <Container className={'relative z-10 block pb-10'}>
